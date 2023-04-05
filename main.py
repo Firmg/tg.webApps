e49c4bc611e096ecdd924af066a0cc78b1474227
Firmg <129497817+Firmg@users.noreply.github.com>
2023-04-05 22:40:17 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types.web_app_info import WebAppInfo
+
+bot = Bot('6054556548:AAFjyVA_-nm9CjK0IK00dAgsd4vSJ68oW8E')
+dp = Dispatcher(bot)
